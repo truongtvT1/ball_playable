e44a94e179165a0d916217b9dde24864e1916ba9
truongtv/HomePC <tavantruong222@gmail.com>
2022-02-28 17:39:49 +0700

    no message

--- Assets/Projects/Scripts/GamePlay/Platform/BrokenPlatform.cs
+++ Assets/Projects/Scripts/GamePlay/Platform/BrokenPlatform.cs
@@ -36,33 +36,33 @@ public class BrokenPlatform : MonoBehaviour
     
     public void OnCollisionEnter2D(Collision2D coll)
     {
-        if (!shaking)
-        {
-            shaking = true;
-            var breakDelayEmission = breakDelayEffect.emission;
-            breakDelayEmission.rateOverTime = 20;
-            foreach (Transform spriteChild in sprite)
-            {
-                // SoundManager.Instance.PlaySound3D(platformBreakingSound, transform.position, 20);
-                spriteChild.DOShakeRotation(breakDelay, new Vector3(0, 0, 20), 30, 20, false).OnComplete(() =>
-                {
-                    // SoundManager.Instance.PlaySound3D(platformExplodeSound, transform.position, 20);
-                    breakDelayEmission.rateOverTime = 0;
-                    breakEffect.Play();
-                    gameObject.SetActive(false);
-                    transform.GetChild(0).eulerAngles = Vector3.zero;
-                    transform.DOScale(1, respawnDelay).OnComplete(() =>
-                    {
-                        // SoundManager.Instance.PlaySound3D(platformAppearSound, transform.position, 20);
-                        var respawnEffectEmission = respawnEffect.emission;
-                        respawnEffectEmission.rateOverTime = 20;
-                        gameObject.SetActive(true);
-                        transform.localScale = Vector3.zero;
-                        transform.DOScale(1, 1f).OnComplete(() => respawnEffectEmission.rateOverTime = 0);
-                        shaking = false;
-                    });
-                });
-            }
-        }
+        // if (!shaking)
+        // {
+        //     shaking = true;
+        //     var breakDelayEmission = breakDelayEffect.emission;
+        //     breakDelayEmission.rateOverTime = 20;
+        //     foreach (Transform spriteChild in sprite)
+        //     {
+        //         // SoundManager.Instance.PlaySound3D(platformBreakingSound, transform.position, 20);
+        //         spriteChild.DOShakeRotation(breakDelay, new Vector3(0, 0, 20), 30, 20, false).OnComplete(() =>
+        //         {
+        //             // SoundManager.Instance.PlaySound3D(platformExplodeSound, transform.position, 20);
+        //             breakDelayEmission.rateOverTime = 0;
+        //             breakEffect.Play();
+        //             gameObject.SetActive(false);
+        //             transform.GetChild(0).eulerAngles = Vector3.zero;
+        //             transform.DOScale(1, respawnDelay).OnComplete(() =>
+        //             {
+        //                 // SoundManager.Instance.PlaySound3D(platformAppearSound, transform.position, 20);
+        //                 var respawnEffectEmission = respawnEffect.emission;
+        //                 respawnEffectEmission.rateOverTime = 20;
+        //                 gameObject.SetActive(true);
+        //                 transform.localScale = Vector3.zero;
+        //                 transform.DOScale(1, 1f).OnComplete(() => respawnEffectEmission.rateOverTime = 0);
+        //                 shaking = false;
+        //             });
+        //         });
+        //     }
+        // }
     }
 }
